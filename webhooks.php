6b2b1ff388f71afd7a1b58dee483d2d97b585515
sunti2002 <sunti.p@gmail.com>
2018-07-29 14:19:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '571e4863f1855c6bce55bddfc65e44ad';
+$access_token = 'd0BomhzWbUf6hYZZurDyZ4SuzEqAtKifunTlCKLm+in9Hui3oTWqrz0P8Dx2GHl++yCHvnKfBQJOG9K/xh0L0mp+ML7ohZf306509nMcUOa8HcSCtgq04BehM3FdJHpXQFS5SKBj/mOK/mzj0qIMxgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
